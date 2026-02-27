97cadc61c2aee1c0fedefafe3aa3a0aa0312f0b2
gaurav <gauravbhasker8@live.com>
2026-02-27 17:55:23 +0530

    removing hardcoded kafka url

--- src/main/java/com/odin/registrationservice/config/AppConfig.java
+++ src/main/java/com/odin/registrationservice/config/AppConfig.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -16,6 +17,9 @@ import com.odin.registrationservice.dto.NotificationDTO;
 
 @Configuration
 public class AppConfig {
+	
+	@Value("${spring.kafka.bootstrap-servers}")
+	private String kafkaUrl; 
 
     @Bean
     public RestTemplate restTemplate() {
@@ -25,7 +29,7 @@ public class AppConfig {
     @Bean
     public ProducerFactory<String, NotificationDTO> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaUrl);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class); 
         return new DefaultKafkaProducerFactory<>(configProps);
